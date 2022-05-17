1261f68e7802a5cc8e2f166b12a900c344f3fb7b
rj_99 <rahuljay2012@gmail.com>
2022-05-17 17:52:22 +0530

    Order Commit

---
+++ OrderItem/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderItem.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public int menuItemId { get; set; }
+        
+
+        public string Name { get; set; }
+    }
+}
